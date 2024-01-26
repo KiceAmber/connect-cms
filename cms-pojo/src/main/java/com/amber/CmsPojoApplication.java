ba8cbbba1e65f259aa73086d1c536fd6247220fb
KiceAmber <kiceamber@gmail.com>
2024-01-26 14:49:06 +0800

    add cms-common cms-pojo cms-server modules to project

---
+++ cms-pojo/src/main/java/com/amber/CmsPojoApplication.java
@@ -0,0 +1,12 @@
+package com.amber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsPojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsPojoApplication.class, args);
+    }
+}
